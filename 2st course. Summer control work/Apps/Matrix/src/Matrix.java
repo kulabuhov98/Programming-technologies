4fc3dcef5f4a25a7612970f7e99b40b10b84d382
kulabuhov98 <kulabuhov98@mail.ru>
2023-06-17 10:19:40 +0300

    Update Matrix.java

--- 2st course. Summer control work/Apps/Matrix/src/Matrix.java
+++ 2st course. Summer control work/Apps/Matrix/src/Matrix.java
@@ -26,16 +26,43 @@ public class Matrix {
         }
     }
 
-    /* Получение матрицы */
+    /* Получение всей матрицы */
     public String getMatrix() {
         /* Получение строкового представления многомерного массива */
         String stringMatrix = Arrays.deepToString(matrix);
         /* Преобразование полученного стрового представления в удобочитаемый формат */
         stringMatrix = stringMatrix.replaceAll("\\[\\[", "[").replaceAll("]]", "]")
                 .replaceAll("], ", "]\n");
-
+        /* Возвращение строкового представления многомерного массива */
         return stringMatrix;
     }
 
-    /* */
+    /* Получение определенного столбца матрицы */
+    public String getColumnMatrix(int column) {
+        /* Массив, содержащий значения столбцов */
+        int[] columnValues = new int[matrix.length];
+        /* Прохождение по строкам матрицы */
+        for (int i = 0; i < matrix.length; i++) {
+            /* Заполнение массива */
+            columnValues[i] = matrix[i][column - 1];
+        }
+        /* Возвращение строкового представления многомерного массива */
+        return Arrays.deepToString(new int[][]{columnValues});
+    }
+
+    /* Получение определенной строки матрицы */
+    public String getRowMatrix(int row) {
+        /* Массив, содержащий значения строк */
+        int[] rowValues = new int[matrix.length];
+        /* Прохождение по столбцам матрицы */
+        for (int j = 0; j < matrix.length; j++) {
+            /* Заполнение массива */
+            rowValues[j] = matrix[row - 1][j];
+        }
+        /* Возвращение строкового представления многомерного массива */
+        return Arrays.deepToString(new int[][]{rowValues});
+    }
+
+    /* Получение главной диагонали матрицы */
+    /* Получение побочной диагонали матрицы */
 }
